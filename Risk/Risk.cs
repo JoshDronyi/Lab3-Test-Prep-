0f638f653fe3966767b7edd14ac8056648425e82
ComradeNapoleon <tue95606@temple.edu>
2016-04-09 16:44:17 -0400

    Empty classes made just so I don't keep getting compiler errors, will keep doing some work on Risk, RiskBoard, and Territory tonight.

---
+++ Risk/Risk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Risk
+{
+    public class Risk
+    {
+        public RiskBoard Board;
+        public List<Player> Players;
+    }
+}

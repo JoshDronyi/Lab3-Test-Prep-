5c2d171e41a825bff94596becf9c510abaad8594
JoshDronyi <joshuadronyi@gmail.com>
2016-04-15 13:01:37 -0400

    Army with subtract classes added

--- Risk/Army.cs
+++ Risk/Army.cs
@@ -44,6 +44,102 @@ namespace Risk
 
             armyUnits.Add(artillary);
         }
+                //Methods to subtract units
+        public void SubtractInfantry() 
+        {
+            //Finds out if there are any loose infantrymen or we would have to break them.
+            List<Unit> infantryList = new List<Unit>();
+
+            for(int i =0; i <armyUnits.Count; i++)
+            {
+                Unit unit =  (Unit) armyUnits[i];
+                if(unit.UnitType == Convert.ToInt32(unitType.infantry))
+                {
+                    infantryList.Add(unit);
+                    armyUnits.Remove(unit);
+                    return;
+                }
+           }
+            //Since there are no single infantry we need to break either a calvary or artillary
+            for (int i = 0; i < armyUnits.Count; i++)
+           {
+                Unit unit = (Unit)armyUnits[i];
+                List<Unit> newInfantry = new List<Unit>();
+                
+                if(unit.UnitType == Convert.ToInt32(unitType.calvary))
+                {
+                    if (armyUnits.Count > 0)
+                    {
+                        newInfantry = BreakCalvary();
+                        SubtractInfantry();
+                    }
+                    else
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+                    
+                }
+                else if (unit.UnitType == Convert.ToInt32(unitType.artillary))
+                {
+                    if (armyUnits.Count > 0)
+                    {
+                        newInfantry = BreakArtillary();
+                        SubtractInfantry();
+                    }
+                    else
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+                }
+           }
+            
+        }
+
+        //Breaks Calvary unit into 5 infantry units
+        public List<Unit> BreakCalvary() 
+        {
+            List<Unit> newInfantry = new List<Unit>();
+            for (int i = 0; i < armyUnits.Count; i++ )
+            {
+                Unit unit = (Unit)armyUnits[i];
+                if(unit.UnitType == Convert.ToInt32(unitType.calvary))
+                {
+                    int count = 0;
+
+                    while (count < 5) 
+                    {
+                        Unit newUnit = new Unit();
+                        newUnit.UnitType = Convert.ToInt32(unitType.infantry);
+                        newInfantry.Add(newUnit);
+                    }
+                }
+
+            }
+            return newInfantry;
+        }
+        
+        //Breaks Artillary unit into 10 infantry units
+        public List<Unit> BreakArtillary() 
+        {
+            List<Unit> newInfantry = new List<Unit>();
+            for (int i = 0; i < armyUnits.Count; i++)
+            {
+                Unit unit = (Unit)armyUnits[i];
+                if (unit.UnitType == Convert.ToInt32(unitType.artillary))
+                {
+                    int count = 0;
+
+                    while (count < 10)
+                    {
+                        Unit newUnit = new Unit();
+                        newUnit.UnitType = Convert.ToInt32(unitType.infantry);
+                        newInfantry.Add(newUnit);
+                    }
+                }
+            }
+            return newInfantry;
+        }
+    
 
         public int GetStrength()
         {
